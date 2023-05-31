c887529be83c1f61b87b7bc31016450b077715de
Kevin Lu <kevinlu1248@gmail.com>
2023-05-30 21:38:47 -0700

    done project

--- src/game_24.py
+++ src/game_24.py
@@ -2,18 +2,18 @@
 
 import itertools
 
-# from tqdm import tqdm
+from tqdm import tqdm
 
 def calculate24(
     numbers: list[int], 
     n = 24,
-    k = 4,
     allow_fraction = True
 ):
     """
     Given a list of k numbers, return True if it is possible to use all k numbers
     and the basic arithmetic operations (+, -, *, /) to obtain the value n.
     """
+    k = len(numbers)
     for perm in itertools.permutations(numbers, k):
         for ops in itertools.product(['+', '-', '*', '/'], repeat=k-1): # get all possible combinations of operators
             curr: float = perm[0]
@@ -32,8 +32,6 @@ def calculate24(
                     curr /= num
             else:
                 if int(curr) == n:
-                    # print(perm)
-                    # print(ops)
                     return True
     return False
 
@@ -44,7 +42,7 @@ assert calculate24([3, 3, 8, 8]) == False
 assert calculate24([4, 4, 4, 4]) == True
 assert calculate24([1, 1, 1, 23]) == True
 
-def check_solvable(k: int = 4):
+def check_solvable(k: int = 4, n: int = 24):
     """
     Check probability of getting something solvable for all k cards
     """
@@ -52,22 +50,16 @@ def check_solvable(k: int = 4):
     cards = [i for i in range(1, max_card)] * 4
     num_cards = len(cards)
     count = 0
-    combinations = num_cards * (num_cards - 1) * (num_cards - 2) * (num_cards - 3) // (k * (k - 1) * (k - 2) * (k - 3))
 
-    cache = {}
-    percent = combinations // 100
+    combinations = 1
+    for i in range(k):
+        combinations *= num_cards - i
+        combinations //= i + 1
     
-    # for i, indices in tqdm(enumerate(itertools.combinations(range(len(cards)), k)), total=combinations):
-    for i, indices in enumerate(itertools.combinations(range(len(cards)), k)):
-        if i % percent == 0:
-            print(f"{i}/{combinations}", end="\r")
-        indices = tuple(sorted(indices))
-        if indices in cache:
-            count += cache[indices]
-        else:
-            cache[indices] = calculate24([cards[i] for i in indices])
-        count += calculate24([cards[i] for i in indices])
+    for indices in tqdm(itertools.combinations(range(len(cards)), k), total=combinations):
+        count += calculate24([cards[i] for i in indices], n=n)
     probability = count / combinations
     return probability
 
-print(check_solvable())
+# print(check_solvable())
+print(check_solvable(6, 144))
